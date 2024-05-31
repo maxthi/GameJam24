0742276d877f97537593034203588ad61006d5b1
maxthi <maxthi@protonmail.com>
2024-05-31 18:47:08 +0200

    input actions for player

--- objects/player/player_script.gd
+++ objects/player/player_script.gd
@@ -1,13 +1,24 @@
 extends CharacterBody2D
 
+const move_speed = 100;
+
 func _physics_process(delta):
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * 30
+
+	
+	
+	if Input.is_action_pressed("move_left"):
+		velocity.x = -move_speed
+	elif Input.is_action_pressed("move_right"):
+		velocity.x = move_speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, 30)
+		velocity.x = move_toward(velocity.x, 0, move_speed)
+	
+	if Input.is_action_pressed("move_up"):
+		velocity.y = -move_speed
+	elif Input.is_action_pressed("move_down"):
+		velocity.y = move_speed
+	else:
+		velocity.y = move_toward(velocity.y, 0, move_speed)
 
 	move_and_slide()
