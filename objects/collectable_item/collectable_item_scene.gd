5ff41db091e0fdfa63a8998485055a18306bb2ac
Joseph Heetel <joseph.heetel@protonmail.com>
2024-06-01 17:14:58 +0200

    Reroute collectible pickup over global singleton. Fix character movement

--- objects/collectable_item/collectable_item_scene.gd
+++ objects/collectable_item/collectable_item_scene.gd
@@ -1,20 +1,31 @@
 @tool # https://docs.godotengine.org/en/latest/tutorials/plugins/running_code_in_the_editor.html#doc-running-code-in-the-editor
+
+class_name CollectibleItem
 extends Area2D
 
-#https://docs.godotengine.org/en/stable/getting_started/step_by_step/signals.html
-signal entered_signal( effectName )
 
-@export var SpriteTexture : Texture :
+@export var SpriteTexture: Texture:
 	set(new_texture): # called in editor
 		SpriteTexture = new_texture # update property variable
 		$Sprite2D.texture = new_texture # update tile node texture
-		
-@export var EffectName : String
+
+
+@export var EffectName: String
+var _hasBeenPickedUp: bool = false
+
 
 func _ready():
 	$Sprite2D.texture = SpriteTexture
 
-func _on_body_entered(body):
-	if body.name == "Player": # this requires player node to be called "Player"
-		entered_signal.emit( EffectName ) # tell the world object was collided
-		queue_free() # delete plant
+
+func reactivateIfWasUsed():
+	_hasBeenPickedUp = false;
+	$Sprite2D.show()
+
+
+func _on_body_entered(body: Node2D):
+	if !_hasBeenPickedUp && body.name == "Player": # this requires player node to be called "Player"
+		var global = get_tree().root.get_child(0) as GlobalScript
+		global.on_collectible_pickup.emit(EffectName)
+		_hasBeenPickedUp = true
+		$Sprite2D.hide()
--- objects/player/player_script.gd
+++ objects/player/player_script.gd
@@ -1,51 +1,53 @@
 extends CharacterBody2D
 
-var MOVE_SPEED = 100;
+var MOVE_SPEED = 100 * 60;
 
 @onready var _animated_sprite = $AnimatedSprite2D
 
 func _ready():
 	_animated_sprite.speed_scale = 2
+	var global = get_tree().root.get_child(0) as GlobalScript
+	global.on_collectible_pickup.connect(_on_collectible_interact)
+
 
 func _physics_process(delta):
 	
+	# Process player input
 	var moveDirection = Vector2(0,0)
 	if Input.is_action_pressed("move_left"):
-		moveDirection.x = -1
-		_animated_sprite.play("walk_left")
-	elif Input.is_action_pressed("move_right"):
-		moveDirection.x = 1
-		_animated_sprite.play("walk_right")
-	
+		moveDirection.x += -1
+	if Input.is_action_pressed("move_right"):
+		moveDirection.x += 1
+
 	if Input.is_action_pressed("move_up"):
-		moveDirection.y = -1
-		_animated_sprite.play("walk_up")
-	elif Input.is_action_pressed("move_down"):
-		moveDirection.y = 1
-		_animated_sprite.play("walk_down")
+		moveDirection.y += -1
+	if Input.is_action_pressed("move_down"):
+		moveDirection.y += 1
 
-	# calculate player velocity
-	var vel = Vector2( moveDirection.x * MOVE_SPEED, moveDirection.y * MOVE_SPEED)
-	
-	# update node member
-	velocity = vel;
+	moveDirection = moveDirection.normalized() # Normalizing a zero vector is fine, returns another zero vector
+
+	# Update walking sprite
+	var spriteName = ""
+	if moveDirection.length_squared() > 0:
+		if abs(moveDirection.y) >= abs(moveDirection.x):
+			if moveDirection.y > 0:
+				spriteName = "walk_down"
+			elif moveDirection.y <= 0:
+				spriteName = "walk_up"
+		else:
+			if moveDirection.x > 0:
+				spriteName = "walk_right"
+			elif moveDirection.x <= 0:
+				spriteName = "walk_left"
+	else:
+		spriteName = "idle_front"
+	_animated_sprite.play(spriteName)
+
+	# calculate player velocity and move
+	velocity = moveDirection * delta * MOVE_SPEED;
 	move_and_slide()
-	
-	# update animation
-	if moveDirection.length() == 0:
-		_animated_sprite.play("idle_front")
 
 
-func _on_plant_1_entered_signal( effectName : String ):
+func _on_collectible_interact( effectName : String ):
 	print( "Run over effect " + effectName )
 	MOVE_SPEED *= 2
-
-
-func _on_plant_2_entered_signal(effectName):
-	print( "Run over effect " + effectName )
-	pass # Replace with function body.
-
-
-func _on_plant_3_entered_signal(effectName):
-	
-	pass # Replace with function body.
